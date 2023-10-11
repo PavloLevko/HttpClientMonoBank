43eb9db2441181082dec2c94b1aaed075178a202
”Pavlo <levkopavlo@gmail.com>
2023-10-11 14:08:28 +0300

    create httpClient

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ import java.net.http.HttpClient;
 
 public class Main {
     public static void main(String[] args) {
-        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpClient httpClient = HttpClient.newBuilder().
+                version(HttpClient.Version.HTTP_2).build();
 
     }
 }
