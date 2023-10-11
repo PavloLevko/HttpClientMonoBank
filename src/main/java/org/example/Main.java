201a69db9815b8964c9554e39fef5aef721bacea
”Pavlo <levkopavlo@gmail.com>
2023-10-11 14:06:30 +0300

    first comit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.net.http.HttpClient;
+
+public class Main {
+    public static void main(String[] args) {
+        HttpClient httpClient = HttpClient.newBuilder().build();
+
+    }
+}
